6f59798afb41b63187fa5b6eee4782222ca55e81
orcunTasdemir <otasdemir@vassar.edu>
2020-09-18 20:00:12 -0400

    initial

---
+++ firstJavaProgram/Greeter.java
@@ -0,0 +1,12 @@
+
+public class Greeter
+{
+    public static void main(String[] args) {
+
+        System.out.println("Hello " + args[0]);
+    }
+
+    public static void sayGoodbye(){
+        System.out.println("Goodbye!");
+    }
+}
